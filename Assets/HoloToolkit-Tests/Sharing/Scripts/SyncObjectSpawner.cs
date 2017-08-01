45a66a03073f732a2c79cdcaa62201ff55cddfc4
renewal-wu <rc3176@hotmail.com>
2017-08-01 15:36:45 +0800

    initial

---
+++ Assets/HoloToolkit-Tests/Sharing/Scripts/SyncObjectSpawner.cs
@@ -0,0 +1,60 @@
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+//
+
+using UnityEngine;
+using HoloToolkit.Sharing.Spawning;
+using HoloToolkit.Unity.InputModule;
+
+namespace HoloToolkit.Sharing.Tests
+{
+    /// <summary>
+    /// Class that handles spawning sync objects on keyboard presses, for the SpawningTest scene.
+    /// </summary>
+    public class SyncObjectSpawner : MonoBehaviour
+    {
+        [SerializeField]
+        private PrefabSpawnManager spawnManager;
+
+        [SerializeField]
+        [Tooltip("Optional transform target, for when you want to spawn the object on a specific parent.  If this value is not set, then the spawned objects will be spawned on this game object.")]
+        private Transform spawnParentTransform;
+
+        private void Awake()
+        {
+            if (spawnManager == null)
+            {
+                Debug.LogError("You need to reference the spawn manager on SyncObjectSpawner.");
+            }
+
+            // If we don't have a spawn parent transform, then spawn the object on this transform.
+            if (spawnParentTransform == null)
+            {
+                spawnParentTransform = transform;
+            }
+        }
+
+        protected void Spawn(SyncSpawnedObject spawnedObject, Vector3 position, Quaternion rotation, string baseName, bool isOwnedLocally)
+        {
+            spawnManager.Spawn(spawnedObject, position, rotation, spawnParentTransform.gameObject, baseName, isOwnedLocally);
+        }
+
+        /// <summary>
+        /// Deletes any sync object that inherits from SyncSpawnObject.
+        /// </summary>
+        public void DeleteSyncObject()
+        {
+            GameObject hitObject = GazeManager.Instance.HitObject;
+            if (hitObject != null)
+            {
+                var syncModelAccessor = hitObject.GetComponent<DefaultSyncModelAccessor>();
+                if (syncModelAccessor != null)
+                {
+                    var syncSpawnObject = (SyncSpawnedObject)syncModelAccessor.SyncModel;
+                    spawnManager.Delete(syncSpawnObject);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/HoloToolkit/Sharing/Scripts/Spawning/SyncSpawnedMewObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using HoloToolkit.Sharing.Spawning;
+using HoloToolkit.Sharing.SyncModel;
+using HoloToolkit.Sharing.Tests;
+using UnityEngine;
+
+namespace HoloToolkit.Sharing.Tests
+{
+    [SyncDataClass]
+    public class SyncSpawnedMewObject : SyncSpawnedObject
+    {
+    }
+}
